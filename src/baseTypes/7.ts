78eac81b713347b2a137931a707b38b8ebc89dd9
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-02-19 15:20:26 +0200

    .

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -3,16 +3,45 @@
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
 
+// enum Day {
+//   WORKING,
+//   DAY_OFF,
+// }
+
+// function isWeekend(day: Day): boolean {
+//   return day === Day.DAY_OFF;
+// }
+
+// const today: Day = Day.WORKING;
+// const isTodayWeekend: boolean = isWeekend(today);
+
+// console.log(isTodayWeekend);
+
+//Варіант 2 
+
 enum Day {
-  WORKING,
-  DAY_OFF,
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY,
+  SUNDAY,
 }
+
+// function isWeekend(day: Day): boolean {
+//   if (day === Day.SATURDAY || day === Day.SUNDAY) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
 
 function isWeekend(day: Day): boolean {
-  return day === Day.DAY_OFF;
+  return day === Day.SATURDAY || day === Day.SUNDAY;
 }
 
-const today: Day = Day.WORKING;
+const today: Day = Day.MONDAY; 
 const isTodayWeekend: boolean = isWeekend(today);
 
 console.log(isTodayWeekend);
